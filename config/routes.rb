4748a1a0556d15813b935594a89f8dd63b891fbc
annr <nosbora@gmail.com>
2016-03-13 21:00:59 -0700

    Revert "add nowrap around app name. change names of dbs. make signup controller and model. make signup form work. make fully responsive switch at 1025."
    This reverts commit 1a89830a744f5b0b72a0d786fd6bf25b4252ec4d.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-     resources :signups
+  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
