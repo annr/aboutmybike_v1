8093650dd27a5502d4ddf176e422eb3cb86e2341
annr <nosbora@gmail.com>
2016-09-17 09:00:15 -0700

    fix test data structure, check in gitignore, develop index to display test data details on a page called bikes.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,28 @@
 const React = require('react');
 const preload = require('json!../fixtures/bikes');
 const _ = require('lodash');
+const BikeListItem = require('./BikeListItem');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      results: _.clone(preload)
+      results: _.clone(preload.Bikes)
     };
   }
   render() {
     return(
-      // <div className="app-container">
-      //   <h1>Bikes</h1>
-      //   <div className="bikes-list">
-      //     {this.state.results.map((el) => {
-      //       return (
-      //         <li>{{el}}</li>
-      //       );
-      //     })}
-      //   </div>
-      // </div>
-      <span>Recently added</span>
+      <div className="mdl-layout__tab-panel is-active" id="overview">
+        {this.state.results.map((el) => {
+          return (
+            <BikeListItem
+              key={el.id}
+              id={el.id}
+              {...el}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
