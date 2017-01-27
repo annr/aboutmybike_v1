ef68e9b82effe2137f1c1d90edc766acfaec3bb2
annr <nosbora@gmail.com>
2017-01-27 09:50:19 -0800

    updated schema with owner manually changed.

--- db/restore.sql
+++ db/restore.sql
@@ -2,12 +2,11 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.1
--- Dumped by pg_dump version 9.6.1
+-- Dumped from database version 9.5.4
+-- Dumped by pg_dump version 9.5.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
--- SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -15,17 +14,18 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
--- COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
 
 SET search_path = public, pg_catalog;
 
@@ -46,8 +46,9 @@ CREATE TABLE bike (
     brakes text,
     speeds smallint,
     handlebars text,
-    created_at timestamp with time zone DEFAULT now(),
-    updated_at timestamp with time zone
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    user_id integer NOT NULL
 );
 
 
@@ -260,42 +261,42 @@ ALTER SEQUENCE user_id_seq OWNED BY "user".id;
 
 
 --
--- Name: bike id; Type: DEFAULT; Schema: public; Owner: arobson
+-- Name: id; Type: DEFAULT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY bike ALTER COLUMN id SET DEFAULT nextval('bike_id_seq'::regclass);
 
 
 --
--- Name: city id; Type: DEFAULT; Schema: public; Owner: arobson
+-- Name: id; Type: DEFAULT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY city ALTER COLUMN id SET DEFAULT nextval('city_id_seq'::regclass);
 
 
 --
--- Name: photo id; Type: DEFAULT; Schema: public; Owner: arobson
+-- Name: id; Type: DEFAULT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY photo ALTER COLUMN id SET DEFAULT nextval('photo_id_seq'::regclass);
 
 
 --
--- Name: story id; Type: DEFAULT; Schema: public; Owner: arobson
+-- Name: id; Type: DEFAULT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY story ALTER COLUMN id SET DEFAULT nextval('story_id_seq'::regclass);
 
 
 --
--- Name: theft id; Type: DEFAULT; Schema: public; Owner: arobson
+-- Name: id; Type: DEFAULT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY theft ALTER COLUMN id SET DEFAULT nextval('theft_id_seq'::regclass);
 
 
 --
--- Name: user id; Type: DEFAULT; Schema: public; Owner: arobson
+-- Name: id; Type: DEFAULT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
@@ -305,13 +306,45 @@ ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regcl
 -- Data for Name: bike; Type: TABLE DATA; Schema: public; Owner: arobson
 --
 
-COPY bike (id, style, brand, model, color, brakes, speeds, handlebars, created_at, updated_at) FROM stdin;
-1	cruiser	Fuji	Cambridge III	red	rim	1	mustache	2016-09-16 18:13:22.682023-07	\N
-3	road	Raleigh	Technium	yellow+gray+pink	rim	12	drop	2016-09-16 18:24:52.781274-07	\N
-4	hybrid	Surly	Straggler	purple+mint	disc	10	drop	2016-09-16 18:27:28.759123-07	\N
-5	fat bike	State Bicycle Company	Megalith	yellow+mint+red	disc	7	riser	2016-09-16 21:31:49.890723-07	\N
-6	bmx	Mongoose	Custon Trials Bike	silver+gold	disc	1	riser	2016-09-16 21:33:55.323714-07	\N
-2	cruiser	Laguna BMX	Super Cruiser	chrome	none	1	other	2016-09-16 18:18:21.902632-07	\N
+COPY bike (id, style, brand, model, color, brakes, speeds, handlebars, created_at, updated_at, user_id) FROM stdin;
+1	cruiser	Fuji	Cambridge III	red	rim	1	mustache	2016-09-16 18:13:22.682023-07	2016-09-27 06:22:32.468796-07	1
+3	road	Raleigh	Technium	yellow+gray+pink	rim	12	drop	2016-09-16 18:24:52.781274-07	2016-09-27 06:22:32.468796-07	1
+4	hybrid	Surly	Straggler	purple+mint	disc	10	drop	2016-09-16 18:27:28.759123-07	2016-09-27 06:22:32.468796-07	1
+2	cruiser	Laguna BMX	Super Cruiser	chrome	none	1	other	2016-09-16 18:18:21.902632-07	2016-09-27 06:22:32.468796-07	1
+6	bmx	Mongoose	Custom Trials Bike	silver+gold	disc	1	riser	2016-09-16 21:33:55.323714-07	2016-09-27 06:22:32.468796-07	1
+7	cruiser	\N	\N	\N	\N	\N	\N	2016-09-27 06:16:00.902921-07	2016-09-27 06:22:32.468796-07	1
+8	cruiser	schwinn	beach	red	rim	7	cruiser	2016-09-27 06:29:27.816243-07	2016-09-27 06:29:27.816243-07	1
+5	fat bike	State Bicycle Company	Megalith Blah Blah Blah	yellow+mint+red	disc	7	riser	2016-09-16 21:31:49.890723-07	2016-09-27 06:22:32.468796-07	1
+9	cruiser	schwinn	beach	red	rim	7	cruiser	2016-10-29 09:49:58.714709-07	2016-10-29 09:49:58.714709-07	1
+11	boo	boo	boo	\N	\N	\N	\N	2016-10-29 13:10:52.281261-07	2016-10-29 13:10:52.281261-07	1
+12	boo	boo	boo	\N	\N	\N	\N	2016-10-29 16:51:26.029358-07	2016-10-29 16:51:26.029358-07	1
+13	Fixed-gear	Trek	Gazelle	\N	\N	\N	\N	2016-10-29 16:56:49.61316-07	2016-10-29 16:56:49.61316-07	1
+14	City	\N	\N	\N	\N	\N	\N	2016-10-29 16:59:33.571513-07	2016-10-29 16:59:33.571513-07	1
+15	Track	\N	\N	\N	\N	\N	\N	2016-10-29 17:00:16.576475-07	2016-10-29 17:00:16.576475-07	1
+16	Road	arobsonoying	3	\N	\N	\N	\N	2016-10-29 17:44:43.843901-07	2016-10-29 17:44:43.843901-07	1
+17	Mountain	Surly	FattyFatty	\N	\N	\N	\N	2016-10-29 17:46:23.746642-07	2016-10-29 17:46:23.746642-07	1
+18	Fixed-gear	Surly	Fatty Fatty	\N	\N	\N	\N	2016-10-29 17:46:58.777008-07	2016-10-29 17:46:58.777008-07	1
+19	Hybrid	motobecane	xxl	\N	\N	\N	\N	2016-10-29 18:15:01.346977-07	2016-10-29 18:15:01.346977-07	1
+20	Mountain			\N	\N	\N	\N	2016-10-30 11:37:58.773074-07	2016-10-30 11:37:58.773074-07	1
+21	boo	boo	boo	\N	\N	\N	\N	2016-10-30 12:09:56.020174-07	2016-10-30 12:09:56.020174-07	1
+22	boo	boo	boo	\N	\N	\N	\N	2016-10-30 12:13:02.722395-07	2016-10-30 12:13:02.722395-07	1
+23	boo	boo	boo	\N	\N	\N	\N	2016-10-30 12:16:44.312318-07	2016-10-30 12:16:44.312318-07	1
+24	boo	boo	boo	\N	\N	\N	\N	2016-10-30 12:17:50.605073-07	2016-10-30 12:17:50.605073-07	1
+25	boo	boo	boo	\N	\N	\N	\N	2016-10-30 12:18:07.137679-07	2016-10-30 12:18:07.137679-07	1
+26	boo	boo	boo	\N	\N	\N	\N	2016-10-30 12:20:36.504461-07	2016-10-30 12:20:36.504461-07	1
+27	Cruiser	Schwinn		\N	\N	\N	\N	2016-10-30 12:28:52.816105-07	2016-10-30 12:28:52.816105-07	1
+28	BMX			\N	\N	\N	\N	2016-10-30 12:44:50.679557-07	2016-10-30 12:44:50.679557-07	1
+29	Cruiser			\N	\N	\N	\N	2016-10-30 12:45:03.055303-07	2016-10-30 12:45:03.055303-07	1
+30	BMX			\N	\N	\N	\N	2016-10-30 12:49:17.129297-07	2016-10-30 12:49:17.129297-07	1
+31	Folding			\N	\N	\N	\N	2016-10-30 13:03:42.645923-07	2016-10-30 13:03:42.645923-07	1
+32	Track			\N	\N	\N	\N	2016-10-30 13:05:24.49138-07	2016-10-30 13:05:24.49138-07	1
+33	Road	motobecane	??	\N	\N	\N	\N	2016-11-03 21:05:09.974824-07	2016-11-03 21:05:09.974824-07	1
+34	Road	motobecane	??	\N	\N	\N	\N	2016-11-03 21:05:18.339264-07	2016-11-03 21:05:18.339264-07	1
+35	Road	Motobecane	??	\N	\N	\N	\N	2016-11-03 21:05:29.459523-07	2016-11-03 21:05:29.459523-07	1
+36	Other	Schwinn		\N	\N	\N	\N	2016-11-03 21:06:08.158754-07	2016-11-03 21:06:08.158754-07	1
+37	boo	boo	boo	\N	\N	\N	\N	2016-11-13 19:38:15.033958-08	2016-11-13 19:38:15.033958-08	1
+38	Cruiser	Schwinn?	uh?	\N	\N	\N	\N	2017-01-23 11:54:20.599836-08	2017-01-23 11:54:20.599836-08	1
+39	Cruiser	Schwinn?	uh?	\N	\N	\N	\N	2017-01-23 11:54:25.186878-08	2017-01-23 11:54:25.186878-08	1
 \.
 
 
@@ -319,7 +352,7 @@ COPY bike (id, style, brand, model, color, brakes, speeds, handlebars, created_a
 -- Name: bike_id_seq; Type: SEQUENCE SET; Schema: public; Owner: arobson
 --
 
-SELECT pg_catalog.setval('bike_id_seq', 6, true);
+SELECT pg_catalog.setval('bike_id_seq', 39, true);
 
 
 --
@@ -387,6 +420,7 @@ SELECT pg_catalog.setval('theft_id_seq', 1, false);
 --
 
 COPY "user" (id, created_at, last_login, username, facebook_id, name, first_name, last_name, facebook_link, gender, locale) FROM stdin;
+1	2016-09-25 20:27:23.210233-07	\N	arobson	123	Ann Robson	Ann	Robson	https://www.facebook.com/aerobson	female	en-us
 \.
 
 
@@ -398,7 +432,7 @@ SELECT pg_catalog.setval('user_id_seq', 1, false);
 
 
 --
--- Name: bike bike_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
+-- Name: bike_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY bike
@@ -406,7 +440,7 @@ ALTER TABLE ONLY bike
 
 
 --
--- Name: city city_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
+-- Name: city_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY city
@@ -414,7 +448,7 @@ ALTER TABLE ONLY city
 
 
 --
--- Name: photo photo_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
+-- Name: photo_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY photo
@@ -422,7 +456,7 @@ ALTER TABLE ONLY photo
 
 
 --
--- Name: story story_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
+-- Name: story_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY story
@@ -430,7 +464,7 @@ ALTER TABLE ONLY story
 
 
 --
--- Name: theft theft_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
+-- Name: theft_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY theft
@@ -438,7 +472,7 @@ ALTER TABLE ONLY theft
 
 
 --
--- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
+-- Name: user_pkey; Type: CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY "user"
@@ -446,7 +480,7 @@ ALTER TABLE ONLY "user"
 
 
 --
--- Name: photo photo_bike_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
+-- Name: photo_bike_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY photo
@@ -454,7 +488,7 @@ ALTER TABLE ONLY photo
 
 
 --
--- Name: photo photo_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
+-- Name: photo_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY photo
@@ -462,7 +496,7 @@ ALTER TABLE ONLY photo
 
 
 --
--- Name: story story_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
+-- Name: story_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY story
@@ -470,7 +504,7 @@ ALTER TABLE ONLY story
 
 
 --
--- Name: theft theft_bike_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
+-- Name: theft_bike_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY theft
@@ -478,7 +512,7 @@ ALTER TABLE ONLY theft
 
 
 --
--- Name: theft theft_owner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
+-- Name: theft_owner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: arobson
 --
 
 ALTER TABLE ONLY theft
@@ -486,5 +520,24 @@ ALTER TABLE ONLY theft
 
 
 --
+-- Name: user_id; Type: FK CONSTRAINT; Schema: public; Owner: arobson
+--
+
+ALTER TABLE ONLY bike
+    ADD CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES "user"(id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
 -- PostgreSQL database dump complete
 --
+
